1b391dab5a63ea06d99e767fb76a0ccc807aa920
Hnoss33 <52913599+Hnoss33@users.noreply.github.com>
2020-08-10 19:27:49 +1000

    ESLint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable indent */
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const HelloWorld = () => {
+return (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+  );
+};
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable eol-last */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
